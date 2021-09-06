0c19126c47278959969b562a4365064d125df480
laille2 <louis.laillet@gmail.com>
2021-09-06 09:12:23 +0200

    Ajout de l'entité Property

---
+++ src/Entity/Property.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+class Property
+{
+    private $id;
+    private $name;
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
