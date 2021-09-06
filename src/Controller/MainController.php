6bf8538f902f6a2bd6474fe6cbed8b7ed77ce4a6
laille2 <louis.laillet@gmail.com>
2021-09-06 09:04:01 +0200

    Ajout Docttrine, Asset, Bootstrap

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,8 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            
+        ]);
     }
 }
